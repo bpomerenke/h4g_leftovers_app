57a9a0e0f3ffef73f1f6848401d8379808442ead
bpom <bpomerenke@gmail.com>
2016-12-10 10:39:03 -0600

    wired up firebase differently

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,6 @@
-angular.module('starter.controllers', [])
+var fb=null;
+
+angular.module('starter.controllers', ['firebase'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -94,19 +96,23 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('SearchController', function($scope){
+.controller('SearchController', function($scope, $firebaseObject){
   $scope.searchResults = [];
   $scope.searchFor = function(searchTerm){
-    $scope.searchResults = [];
-    return firebase.database().ref('/providers').once('value').then(function(snapshot) {
-      var providers = snapshot.val();
-      var count = 0;
-      for(idx in providers){
-        console.log(providers[idx]);
-        $scope.searchResults.push(providers[idx]);
-        if(count++ > searchTerm.length) break;
-      }
-      $scope.$apply();
-    });
+    fb = new Firebase("https://elvis-2ae10.firebaseio.com");
+
+    var syncObject = $firebaseObject(fb.child("providers/"));
+    syncObject.$bindTo($scope, "searchResults");
+    // $scope.searchResults = [];
+    // return firebase.database().ref('/providers').once('value').then(function(snapshot) {
+    //   var providers = snapshot.val();
+    //   var count = 0;
+    //   for(idx in providers){
+    //     console.log(providers[idx]);
+    //     $scope.searchResults.push(providers[idx]);
+    //     if(count++ > searchTerm.length) break;
+    //   }
+    //   $scope.$apply();
+    // });
   };
 });
