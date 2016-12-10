d19b8a99a105b01899f312733abe0cff873a775a
bpom <bpomerenke@gmail.com>
2016-12-10 01:29:24 -0600

    fixed dup controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -46,27 +46,28 @@ angular.module('starter.controllers', [])
     {
       active: false,
       iconActive: 'ion-sad', // icon reference http://ionicons.com/
-     iconInactive: 'ion-happy-outline',
-    label: 'Are you ok?',
-    needs: [
-      { label: "I'm hungry", needCode:'fud'},
-      { label: "I'm sick or in pain", needCode:'med', emergencyConcern:true},
-      { label: "I need somewhere to go", needCode:'shl'}]
-  },{
-    active:false,
-    iconActive: 'ion-ios-chatboxes',
-    iconInactive: 'ion-ios-chatboxes-outline',
-    label: 'Need to talk?',
-    subLabel: 'What do you need help with?',
-    needs: [
+      iconInactive: 'ion-happy-outline',
+      label: 'Are you ok?',
+      needs: [
+        { label: "I'm hungry", needCode:'fud'},
+        { label: "I'm sick or in pain", needCode:'med', emergencyConcern:true},
+        { label: "I need somewhere to go", needCode:'shl'}]
+    },
+    {
+      active:false,
+      iconActive: 'ion-ios-chatboxes',
+      iconInactive: 'ion-ios-chatboxes-outline',
+      label: 'Need to talk?',
+      subLabel: 'What do you need help with?',
+      needs: [
       //TODO: Utilize emergencyConcern as signal to interrupt and check if there is an emergency
-      { label: 'Medical problems', needCode:'med', emergencyConcern:true},
-      { label: 'Abuse or violence', needCode:'abv', emergencyConcern:true},
-      { label: 'Mental or emotional', needCode:'psy'},
-      { label: 'Drugs or alcohol', needCode:'sub'},
-      { label: 'Work or money', needCode:'fin'},
-      { label: 'Support and advice', needCode:'adv'}]
-  }
+        { label: 'Medical problems', needCode:'med', emergencyConcern:true},
+        { label: 'Abuse or violence', needCode:'abv', emergencyConcern:true},
+        { label: 'Mental or emotional', needCode:'psy'},
+        { label: 'Drugs or alcohol', needCode:'sub'},
+        { label: 'Work or money', needCode:'fin'},
+        { label: 'Support and advice', needCode:'adv'}]
+    }
   ];
 
   $scope.toggleGroup = function(group) {
@@ -76,7 +77,4 @@ angular.module('starter.controllers', [])
   $scope.isGroupShown = function(group) {
     return $scope.shownGroup === group;
   };
-})
-
-.controller('NeedsCtrl', function($scope, $stateParams) {
 });
