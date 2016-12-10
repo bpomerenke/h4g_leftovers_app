a7e076005da166d2f41d95bad1c43941a349cb7b
Josh Sutterfield <Josh Sutterfield>
2016-12-10 11:03:32 -0600

    search restored

--- www/js/search.js
+++ www/js/search.js
@@ -3,10 +3,10 @@ var fb=null;
 angular.module('starter.controllers').controller('SearchController', function($scope, $firebaseObject){
   $scope.searchResults = [];
   $scope.searchFor = function(searchTerm){
-    //fb = new Firebase("https://elvis-2ae10.firebaseio.com");
+    fb = new Firebase("https://elvis-2ae10.firebaseio.com");
 
-    //var syncObject = $firebaseObject(fb.child("providers/"));
-    //syncObject.$bindTo($scope, "searchResults");
+    var syncObject = $firebaseObject(fb.child("providers/"));
+    syncObject.$bindTo($scope, "searchResults");
     // $scope.searchResults = [];
     // return firebase.database().ref('/providers').once('value').then(function(snapshot) {
     //   var providers = snapshot.val();
