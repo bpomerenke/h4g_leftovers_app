0baac853e61dc9c8df6500346f681b6c3c5f6f7d
Kevin Wyas <kevin@connectionsdesing.com>
2016-12-10 09:47:50 -0600

    browse pulling data from fb

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,16 +42,14 @@ angular.module('starter.controllers', [])
 })
 
 .controller('BrowseCtrl', function($scope){
-  $scope.providerList = [];
+  console.log('browse control init');
     $scope.searchResults = [];
     return firebase.database().ref('/providerCategories').once('value').then(function(snapshot) {
-      console.log("getting categories ")
       var providerCategories = snapshot.val();
       var count = 0;
       for(idx in providerCategories){
         console.log(providerCategories[idx]);
-        $scope.searchResults.push(providers[idx]);
-        if(count++ > searchTerm.length) break;
+        $scope.searchResults.push(providerCategories[idx]);
       }
       $scope.$apply();
     });
